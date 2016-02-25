9e6af1ccb0b600279465e8f91771c94298f9b481
Randolph Voorhies <rand.voorhies@gmail.com>
2016-02-24 22:51:50 -0800

    Basic output is starting to come together

--- trainingparser.py
+++ trainingparser.py
@@ -1,12 +1,17 @@
 import sys
 from pprint import pprint
 import re
+import yattag
 
 maxes = {
     'snatch': 100,
     'c&j': 120,
     'squat': 160
 }
+
+######################################################################
+# Parsing
+######################################################################
 
 infile = open(sys.argv[1], 'r')
 
@@ -21,8 +26,6 @@ for lineNumber, line in enumerate(infile):
     try:
         # Strip out any leading and trailing whitespace
         line = line.strip()
-
-        # print line
 
         # If the line starts with "Week", then let's start a new week
         if line.lower().startswith('week'):
@@ -121,8 +124,12 @@ for lineNumber, line in enumerate(infile):
                 currentDay['lifts'].append(currentLift)
 
                 # print line
+            elif line.lower() == 'off':
+                currentDay['lifts'] = None
             else:
-                currentDay['lifts'].append({'name': line})
+                raise RuntimeError('Error parsing line {} (No ":" found): "{}"'.format(
+                    lineNumber + 1, line))
+                # currentDay['lifts'].append({'name': line})
     except:
         print 'Error parsing line {}: "{}"'.format(lineNumber + 1, line)
         raise
@@ -131,3 +138,89 @@ for lineNumber, line in enumerate(infile):
 weeks.append(currentWeek)
 
 # pprint(weeks)
+
+######################################################################
+# Output
+######################################################################
+
+doc, tag, text = yattag.Doc().tagtext()
+
+with tag('html'):
+    with tag('head'):
+        doc.asis(r"""
+             <style type="text/css">
+                 @media all {
+                 .page-break	{ display: none; }
+                 }
+
+                 @media print {
+                 .page-break	{ display: block; page-break-before: always; }
+                 }
+             </style>
+             """)
+    with tag('body'):
+        with tag('h1'):
+            text('Training Log')
+
+        for weekNumber, week in enumerate(weeks):
+
+            # Count the maximum number of sets for any exercise this week
+            maxSets = 0
+            for day in week['days']:
+                if day['lifts'] is not None:
+                    for lift in day['lifts']:
+                        if isinstance(lift['sets'], list):
+                            maxSets = max(maxSets, len(lift['sets']))
+
+            # Print the name of the week
+            with tag('h2'):
+                text(''.format(week['name']))
+
+            with tag('table', border=1):
+
+                # Print the table header
+                with tag('tr'):
+                    with tag('th'):
+                        text('Day')
+                    with tag('th'):
+                        text('Exercise')
+
+                    # Print the set number headers
+                    for setNum in range(1, maxSets + 1):
+                            with tag('th'):
+                                text('Set {}'.format(setNum))
+
+                for day in week['days']:
+                    # Calculate the number of lifts for this day (or 1 if it is an off day)
+                    numLifts = len(day['lifts']) if day['lifts'] is not None else 1
+
+                    # Write out the day cell which covers numLifts rows
+                    with tag('tr'):
+                        with tag('td', rowspan=numLifts + 1):
+                            text(day['name'])
+
+                    if day['lifts'] is not None:
+
+                        # For each lift in the day, make a new row
+                        for lift in day['lifts']:
+                            with tag('tr'):
+                                with tag('td'):
+                                    text(lift['name'])
+
+                                for i in range(0, maxSets):
+                                    with tag('td'):
+                                        text('hi')
+                    else:
+                        # If lifts is None then make this an "Off" day
+                        with tag('tr'):
+                            with tag('td', colspan=maxSets + 1):
+                                text('Off')
+
+            # Suggest a page-break for printers
+            if weekNumber < len(weeks) - 1:
+                with tag('div', klass='page-break'):
+                    pass
+
+
+with open('output.html', 'w') as outfile:
+    outfile.write(doc.getvalue())
